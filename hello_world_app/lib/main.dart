423f2b573224099ef25203d306628980a9d9a8f5
Luis Mejia <lmejia@siesa.com>
2025-03-21 07:50:25 -0500

    feat: usar Transform.scale, para aumentar el tamaño del texto hola mundo

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -9,10 +9,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(child: Text("Hola mundo"))
+        body: Center(
+          child: Transform.scale(
+            scale: 4,
+            child: Text("Hola mundo")
+          )
+        ) 
       )
     );
   }
