a49d84f6915ea96f285b786b008ed48769fc9a1c
Luis Mejia <lmejia@siesa.com>
2025-03-21 16:30:09 -0500

    feat: Comportamiento del FormField

--- yes_no_app/lib/presentation/widgets/chat/shared/message_field_box.dart
+++ yes_no_app/lib/presentation/widgets/chat/shared/message_field_box.dart
@@ -6,7 +6,8 @@ class MessageFieldBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
-    // final colors = Theme.of(context).colorScheme;
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
 
     final outlineInputBorder = UnderlineInputBorder(
       borderSide: const BorderSide( color: Colors.transparent ),
@@ -15,31 +16,31 @@ class MessageFieldBox extends StatelessWidget {
 
 
     final inputDecoration = InputDecoration (  
-      /*
-      enabledBorder: OutlineInputBorder(
-        borderSide: BorderSide( color: colors.primary),
-        borderRadius: BorderRadius.circular(10)
-      ),
-      */
-
+      hintText: "End your message with a '?'",
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
       filled: true,
       suffixIcon: IconButton(
         icon: Icon(Icons.send_outlined),
         onPressed: () {
-          print("valor caja texto");
+          final textValue = textController.value.text;
+          print("button: $textValue");
+          textController.clear();
         } ),
     );
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print("onFieldSubmitted: $value");
-      },
-      onChanged: (value) {
-        print("onChanged: $value");
-      },
+        textController.clear();
+        focusNode.requestFocus();
+      }
     );
   }
 }
