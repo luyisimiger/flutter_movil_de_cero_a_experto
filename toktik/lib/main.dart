bf71b139f7e25d1ca95fe4bb4681b5decd8fb107
Luis Mejia <lmejia@siesa.com>
2025-03-23 17:33:26 -0500

    feat: Usando el repositorio y origen de datos

--- toktik/lib/main.dart
+++ toktik/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
+import 'package:toktik/infrastructure/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infrastructure/repositorires/video_post_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
@@ -11,11 +14,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final videoRepository = VideoPostRepositoryImpl(
+      videoDatasource: LocalVideoDatasourceImpl()
+    );
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           // lazy: false,
-          create: (_) => DiscoverProvider()..loadNextPage()
+          create: (_) => DiscoverProvider(videoRepository: videoRepository)..loadNextPage()
         ),
       ],
       child: MaterialApp(
