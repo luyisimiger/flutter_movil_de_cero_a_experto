620a0b698e1f5bf39cd6581a32b7fa177ec72668
Luis Mejia <lmejia@siesa.com>
2025-03-23 07:00:58 -0500

    feat: Mostrar los mensajes del provider

--- yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/shared/message_field_box.dart';
@@ -30,6 +33,9 @@ class _ChatView extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+    
+    final chatProvider = context.watch<ChatProvider>();
+    
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -37,9 +43,10 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
               child: ListView.builder(
-                itemCount: 100,
+                itemCount: chatProvider.messages.length,
                 itemBuilder: (context, index) {
-                  return (index % 2 == 0)
+                  final message = chatProvider.messages[index];
+                  return ( message.type == MessageType.others )
                   ? const HerMessageBubble()
                   : const MyMessageBubble();
                 },
