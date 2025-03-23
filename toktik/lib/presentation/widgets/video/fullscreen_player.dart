9fb889d09188369c03a106cff68d781b593d1ad8
Luis Mejia <lmejia@siesa.com>
2025-03-23 16:04:59 -0500

    feat: Pausar videos

--- toktik/lib/presentation/widgets/video/fullscreen_player.dart
+++ toktik/lib/presentation/widgets/video/fullscreen_player.dart
@@ -53,21 +53,27 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
           return const Center(child: CircularProgressIndicator(strokeWidth: 3,));
         }
         
-        return AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: Stack(
-            children: [
-              VideoPlayer(controller),
-
-              // gradiente
-              
-              // texto
-              Positioned(
-                bottom: 50,
-                left: 20,
-                child: _VideoCaption(caption: widget.caption)
-              ),
-            ]
+        return GestureDetector(
+          onTap: () {
+            if (controller.value.isPlaying) controller.pause();
+            else controller.play();
+          },
+          child: AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: Stack(
+              children: [
+                VideoPlayer(controller),
+          
+                // gradiente
+                
+                // texto
+                Positioned(
+                  bottom: 50,
+                  left: 20,
+                  child: _VideoCaption(caption: widget.caption)
+                ),
+              ]
+            ),
           ),
         );
       },
