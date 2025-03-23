67cf9d3efdff7e78d0dd80554404674887846a1b
Luis Mejia <lmejia@siesa.com>
2025-03-23 06:36:03 -0500

    feat: Provider - Gestor de estado

---
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  
+  List<Message> messages = [
+    Message(text: "mensaje 1", type: MessageType.mine),
+    Message(text: "mensaje 2", type: MessageType.others),
+    Message(text: "mensaje 3", type: MessageType.others),
+    Message(text: "mensaje 4", type: MessageType.mine),
+    Message(text: "mensaje 5", type: MessageType.others)
+  ];
+
+  void sendMessage( String text) async {
+    // TODO: implementar
+  }
+}
